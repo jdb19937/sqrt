31c5a1fe3369854f2029097fac913169665da5f0
Dan Brumleve <jdb1729@gmail.com>
2026-04-05 01:24:27 -0500

    linte

--- visio.c
+++ visio.c
@@ -140,7 +140,7 @@ static void navis_reddere(unsigned char *fenestra, const visio_navis_t *n)
                 double zi2 = 2.0 * azr * azi + ci;
                 zr         = zr2;
                 zi         = zi2;
-                double r2 = zr * zr + zi * zi;
+                double r2  = zr * zr + zi * zi;
                 if (r2 > 256.0 || r2 != r2)
                     break;
                 iter++;
@@ -156,46 +156,49 @@ static void navis_reddere(unsigned char *fenestra, const visio_navis_t *n)
             } else {
                 /* smooth coloring */
                 double r2 = zr * zr + zi * zi;
-                if (r2 < 1.0) r2 = 1.0;
+                if (r2 < 1.0)
+                    r2 = 1.0;
                 double lr = log2(r2);
-                if (lr < 1.0) lr = 1.0;
+                if (lr < 1.0)
+                    lr = 1.0;
                 double mu = (double)iter - log2(lr) + 4.0;
                 double t  = mu / cyc + pha;
 
                 /* palette bichroma: caeruleum profundum ↔ ignis aureus */
                 double s = fmod(t, 1.0);
-                if (s < 0.0) s += 1.0;
+                if (s < 0.0)
+                    s += 1.0;
                 double r, g, b;
                 if (s < 0.15) {
                     /* nigrum → caeruleum profundum */
                     double q = s / 0.15;
-                    r = 0.02 * q;
-                    g = 0.04 * q;
-                    b = 0.18 * q;
+                    r        = 0.02 * q;
+                    g        = 0.04 * q;
+                    b        = 0.18 * q;
                 } else if (s < 0.4) {
                     /* caeruleum profundum → caeruleum lucidum */
                     double q = (s - 0.15) / 0.25;
-                    r = 0.02 + 0.08 * q;
-                    g = 0.04 + 0.30 * q;
-                    b = 0.18 + 0.55 * q;
+                    r        = 0.02 + 0.08 * q;
+                    g        = 0.04 + 0.30 * q;
+                    b        = 0.18 + 0.55 * q;
                 } else if (s < 0.55) {
                     /* caeruleum → aurantium (transitus) */
                     double q = (s - 0.4) / 0.15;
-                    r = 0.10 + 0.70 * q;
-                    g = 0.34 - 0.04 * q;
-                    b = 0.73 - 0.68 * q;
+                    r        = 0.10 + 0.70 * q;
+                    g        = 0.34 - 0.04 * q;
+                    b        = 0.73 - 0.68 * q;
                 } else if (s < 0.8) {
                     /* aurantium → flavum lucidum */
                     double q = (s - 0.55) / 0.25;
-                    r = 0.80 + 0.20 * q;
-                    g = 0.30 + 0.50 * q;
-                    b = 0.05 + 0.05 * q;
+                    r        = 0.80 + 0.20 * q;
+                    g        = 0.30 + 0.50 * q;
+                    b        = 0.05 + 0.05 * q;
                 } else {
                     /* flavum → album → nigrum (reditus) */
                     double q = (s - 0.8) / 0.2;
-                    r = 1.00 - 0.85 * q;
-                    g = 0.80 - 0.70 * q;
-                    b = 0.10 - 0.05 * q;
+                    r        = 1.00 - 0.85 * q;
+                    g        = 0.80 - 0.70 * q;
+                    b        = 0.10 - 0.05 * q;
                 }
                 r *= sat;
                 g *= sat;
